26a49ed6652f83bd9b8573a8821b04e6b7c20614
Marc Evanstein <marc@marcevanstein.com>
2026-09-24 14:27:40 -0700

    Updated stale information in main docstring

--- src/scamp_extensions/__init__.py
+++ src/scamp_extensions/__init__.py
@@ -20,10 +20,12 @@ framework for music composition. This package is the place for models of music-t
 pitch-class sets), conveniences for interacting with various types of input and output, and in general anything that
 builds upon SCAMP but is outside of the scope of the main framework.
 
-The package is split into several subpackages according to the nature of the content: The pitch subpackage contains
-tools for manipulating pitches, such as scales and intervals; the rhythm subpackage contains tools for creating and
-interacting with rhythms and meters; the interaction subpackage contains utilities for human interface devices and
-other live interactions; and the composers subpackage contains composer-specific tools and theoretical devices.
+The package is split into several subpackages: the pitch subpackage has tools for manipulating pitches, such as
+scales and intervals; rhythm has tools for representing meters and rhythmic structures; playback has extensions such as
+multi-preset instruments; engraving has extensions such as graphic notation; interaction has utilities for human
+interface devices and other live interactions; process has tools for modeling a compositional process, such as Markov
+chains and L-systems; composers showcases some composer-specific tools and theoretical devices; parsing has utilities
+for parsing data formats; and utilities has general helpers that fit nowhere else.
 """
 
 import importlib.metadata
